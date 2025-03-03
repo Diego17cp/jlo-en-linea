aea242a0c705ad2683003b6f82ca3fa04d25b166
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-03 11:26:11 -0500

    refactor: move color definitions to utils and add sample link data

---
+++ project2mdjlo/src/utils/index.ts
@@ -0,0 +1,33 @@
+export const colors = [
+    "#3498db",
+    "#2ecc71",
+    "#9b59b6",
+    "#e74c3c",
+    "#f1c40f",
+    "#1abc9c",
+    "#34495e",
+    "#e67e22",
+    "#5DADE2",
+    "#45B39D" 
+]
+
+export const linksData = [
+    {
+        icon: "fab fa-github",
+        title: "Github",
+        desc: "My Github Profile",
+        href: "https://github.com"
+    },
+    {
+        icon: "fab fa-linkedin",
+        title: "LinkedIn",
+        desc: "My LinkedIn Profile",
+        href: "https://linkedin.com"
+    },
+    {
+        icon: "fab fa-twitter",
+        title: "Twitter",
+        desc: "My Twitter Profile",
+        href: "https://twitter.com"
+    }
+]
