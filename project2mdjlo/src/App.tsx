927580e0413d19dcae102205dbdc678c26550ea0
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-03-03 11:50:39 -0500

    change

--- project2mdjlo/src/App.tsx
+++ project2mdjlo/src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/header";
-
+import { linksData } from "./utils";
+import { LinkList } from "./components/LinkList";
 function App() {
   return (
     <div>
       <Header />
+      <LinkList data={linksData} />
     </div>
   );
 }
