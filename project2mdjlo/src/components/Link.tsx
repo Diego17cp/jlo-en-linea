04a0566a7b31eb48a2fa179f43151586f059053e
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-03 11:14:50 -0500

    add random color styling to Link component

--- project2mdjlo/src/components/Link.tsx
+++ project2mdjlo/src/components/Link.tsx
@@ -4,10 +4,25 @@ export interface LinkProps {
 	desc: string;
 	href: string;
 }
+const colors = [
+    "#3498db",
+    "#2ecc71",
+    "#9b59b6",
+    "#e74c3c",
+    "#f1c40f",
+    "#1abc9c",
+    "#34495e",
+    "#e67e22",
+    "#5DADE2",
+    "#45B39D" 
+]
+const randomColor = colors[Math.floor(Math.random() * colors.length)]
 export const Link = ({ icon, title, desc, href }: LinkProps) => {
 	return (
 		<div className="link-item">
-			<a href={href} className="link">
+			<a href={href} className="link" style={{
+                color: randomColor
+            }}>
 				<i className={icon} />
 			</a>
 			<div className="link-content">
