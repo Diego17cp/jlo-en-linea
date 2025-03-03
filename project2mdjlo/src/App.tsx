554a83a05f31af3b4513e6dbe120365287cb9452
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-03 12:01:19 -0500

    style: format code and improve LinkList component structure

--- project2mdjlo/src/App.tsx
+++ project2mdjlo/src/App.tsx
@@ -1,13 +1,14 @@
 import { Header } from "./components/header";
 import { linksData } from "./utils";
 import { LinkList } from "./components/LinkList";
+import "./App.css";
 function App() {
-  return (
-    <div>
-      <Header />
-      <LinkList data={linksData} />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<LinkList data={linksData} />
+		</div>
+	);
 }
 
 export default App;
--- project2mdjlo/src/components/LinkList.tsx
+++ project2mdjlo/src/components/LinkList.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Link } from './Link';
+import { Link } from "./Link";
 interface LinkProps {
-    icon: string;
-    title: string;
-    href: string;
-    desc: string;
+	icon: string;
+	title: string;
+	href: string;
+	desc: string;
 }
-type LinkListProps = LinkProps[];
+interface LinkListProps {
+	data: LinkProps[];
+}
 
-export const LinkList: React.FC<{ data: LinkListProps }> = ({ data }) => {
-    return (
-        <ul>
-            {data.map((link, index) => (
-                <Link key={index} icon={link.icon} title={link.title} href={link.href} desc={link.desc}>
-                </Link>
-            ))}
-        </ul>
-    );
+export const LinkList = ({ data }: LinkListProps) => {
+	return (
+		<div className="link-list">
+			{data.map((link, index) => (
+				<Link
+					key={link.title + index}
+					icon={link.icon}
+					title={link.title}
+					href={link.href}
+					desc={link.desc}
+				></Link>
+			))}
+		</div>
+	);
 };
