f053d9c57944d228d400f5ead26c3d5498ac69bb
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 09:25:22 -0500

    style: refactor header component for improved structure and styling

--- project2mdjlo/src/components/header.tsx
+++ project2mdjlo/src/components/header.tsx
@@ -1,22 +1,25 @@
 import "../header.css";
 
 export const Header = () => {
-  return (
-    <header className="encabezado">
-      <div className="contenedor">
-        {/* Logo y nombre de la entidad */}
-        <div className="logo-titulo">
-          <img src="/logo.jpeg" alt="Logo" className="logo" />
-          <h1 className="titulo">Municipalidad Distrital de José Leonardo Ortiz</h1>
-        </div>
-
-        {/* Botón de acción */}
-        <div className="contenedor-boton">
-          <button className="boton boton-secundario">Registra tu asistencia</button>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className="header">
+            <nav className="nav-container">
+                <div className="nav-content">
+                    <div className="logo-section">
+                        <img 
+                            src="/logo.jpeg" 
+                            alt="Logo Municipal" 
+                            className="nav-logo"
+                        />
+                        <h3 className="nav-title">
+                            <span>Municipalidad Distrital de</span>
+                            <span>José Leonardo Ortiz</span>
+                        </h3>
+                    </div>
+                </div>
+            </nav>
+        </header>
+	);
 };
 
 export default Header;
