985c672a1afef66b793f3f6cd6195afe41b4dcd8
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 12:12:48 -0500

    refactor: comment out unused Section route in App component

--- project2mdjlo/src/App.tsx
+++ project2mdjlo/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
 import "./App.css";
+import { Layout } from "./components/Layout";
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Home />}></Route>
-					<Route path="seccion/:id" element={<Section />}></Route>
+					{/* <Route path="seccion/:id" element={<Section />}></Route> */}
 				</Route>
 			</Routes>
 		</BrowserRouter>
