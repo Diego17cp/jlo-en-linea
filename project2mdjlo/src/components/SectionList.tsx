7cc6f4528e340932ac0a18889f2c8a7bcde8a4df
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 11:01:38 -0500

    feat: add LinkList component for rendering a list of sections with dynamic data

---
+++ project2mdjlo/src/components/SectionList.tsx
@@ -0,0 +1,27 @@
+import { Section } from "./Section";
+
+interface SectionProps {
+    icon: string;
+    title: string;
+    href: string;
+    desc: string;
+}
+interface SectionListProps {
+    data: SectionProps[];
+}
+
+export const LinkList = ({ data }: SectionListProps) => {
+    return (
+        <div className="link-list">
+            {data.map((link, index) => (
+                <Section
+                    key={link.title + index}
+                    icon={link.icon}
+                    title={link.title}
+                    href={link.href}
+                    desc={link.desc}
+                ></Section>
+            ))}
+        </div>
+    );
+};
