6a67c3a742186dee9080281610d8feb3ef09fa2d
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 16:36:47 -0500

    refactor: adjust font size in LinkItem and improve layout structure

--- project2mdjlo/src/components/LinkItem.tsx
+++ project2mdjlo/src/components/LinkItem.tsx
@@ -8,10 +8,18 @@ export interface LinkProps {
 }
 
 export const Link = ({ icon, title, desc, href }: LinkProps) => {
-	const [randomColor] = useState<string>(colors[Math.floor(Math.random() * colors.length)]);
+	const [randomColor] = useState<string>(
+		colors[Math.floor(Math.random() * colors.length)]
+	);
 	return (
-		<div className="link-item bg-transparent p-4 flex justify-between items-center flex-col">
-			<a href={href} className={`bg-(--bg-body) text-6xl w-24 h-24 flex justify-center items-center rounded-full transition-all duration-300 ease-in-out link ${randomColor}`} target="_blank" rel="noreferrer" title={title}>
+		<div className="link-item bg-transparent p-4 flex items-center flex-col">
+			<a
+				href={href}
+				className={`bg-(--bg-body) text-6xl w-24 h-24 flex justify-center items-center rounded-full transition-all duration-300 ease-in-out link ${randomColor}`}
+				target="_blank"
+				rel="noreferrer"
+				title={title}
+			>
 				<i className={icon} />
 			</a>
 			<div className="flex justify-center items-center flex-col gap-2 mt-6 link-content">
