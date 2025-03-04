95c78629864eb70ab9d7cc4fa6cdf553e4d992d3
Julian Burga Bracamonte <julianburgabracamonte@gmail.com>
2025-03-04 09:48:48 -0500

    Update header.tsx

--- project2mdjlo/src/components/header.tsx
+++ project2mdjlo/src/components/header.tsx
@@ -1,17 +1,17 @@
-import "../header.css";
+// import "../header.css";
 
 export const Header = () => {
 	return (
-		<header className="header">
-            <nav className="nav-container">
-                <div className="nav-content">
-                    <div className="logo-section">
+		<header className="w-full bg-white shadow-md py-4">
+            <nav className="mx-auto px-4 md:px-2">
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-4">
                         <img 
                             src="/logo.jpeg" 
                             alt="Logo Municipal" 
-                            className="nav-logo"
+                            className="w-12 h-16 md:w-15 md:h-18 object-contain"
                         />
-                        <h3 className="nav-title">
+                        <h3 className="flex flex-col m-0 text-secondary font-titles font-medium text-lg md:text-base">
                             <span>Municipalidad Distrital de</span>
                             <span>José Leonardo Ortiz</span>
                         </h3>
