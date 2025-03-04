a5c4bb544d3068180485fd9793be01f0a219722c
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 15:27:01 -0500

    feat: enable section routing and implement Section component for dynamic link rendering

--- project2mdjlo/src/App.tsx
+++ project2mdjlo/src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
 import "./App.css";
 import { Layout } from "./components/Layout";
+import { Section } from "./pages/Section";
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Home />}></Route>
-					{/* <Route path="seccion/:id" element={<Section />}></Route> */}
+					<Route path="seccion/:id" element={<Section />}></Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
--- project2mdjlo/src/pages/Section.tsx
+++ project2mdjlo/src/pages/Section.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router';
 import { linkData } from '../utils';
+import { LinkList } from '../components/LinkList';
 
 export const Section = () => {
-    const { id } = useParams<{ id: string }>();
+    const { id } = useParams();
     const links = linkData[id as keyof typeof linkData] || [];
 
     return <LinkList data={links} />;
