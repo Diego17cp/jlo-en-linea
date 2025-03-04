dfbe3092aa87075424ddb42ccb7c72d163b641fb
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 16:40:15 -0500

    refactor: remove unused linksData array from utils

--- project2mdjlo/src/utils/index.ts
+++ project2mdjlo/src/utils/index.ts
@@ -11,56 +11,6 @@ export const colors = [
     "text-green-400"
 ]
 
-export const linksData = [
-    {
-        icon: "fa-solid fa-users-between-lines",
-        title: "Github",
-        desc: "My Github Profile",
-        href: "https://github.com"
-    },
-    {
-        icon: "fab fa-linkedin",
-        title: "LinkedIn",
-        desc: "My LinkedIn Profile",
-        href: "https://linkedin.com"
-    },
-    {
-        icon: "fab fa-twitter",
-        title: "Twitter",
-        desc: "My Twitter Profile",
-        href: "https://twitter.com"
-    },
-    {
-        icon: "fab fa-instagram",
-        title: "Instagram",
-        desc: "My Instagram Profile",
-        href: "https://instagram.com"
-    },
-    {
-        icon: "fab fa-facebook",
-        title: "Facebook",
-        desc: "My Facebook Profile",
-        href: "https://facebook.com"
-    },
-    {
-        icon: "fab fa-youtube",
-        title: "YouTube",
-        desc: "My YouTube Channel",
-        href: "https://youtube.com"
-    },
-    {
-        icon: "fab fa-pinterest",
-        title: "Pinterest",
-        desc: "My Pinterest Profile",
-        href: "https://pinterest.com"
-    },
-    {
-        icon: "fab fa-tiktok",
-        title: "TikTok",
-        desc: "My TikTok Profile",
-        href: "https://tiktok.com"
-    }
-]
 export const sectionData = [
     {
         id: 'consultas',
