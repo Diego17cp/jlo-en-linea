cb3037952fbe8fa65023dc0158fbb547c2e269d3
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 11:00:20 -0500

    feat: add Section component for dynamic link display with random colors

---
+++ project2mdjlo/src/components/Section.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { colors } from "../utils";
+
+export interface SectionProps {
+	icon: string;
+	title: string;
+	desc: string;
+	href: string;
+}
+
+export const Section = ({ icon, title, desc, href }: SectionProps) => {
+    const [randomColor] = useState<string>(colors[Math.floor(Math.random() * colors.length)]);
+    return (
+        <div className="link-item">
+            <a href={href} className={`link ${randomColor}`} target="_blank" rel="noreferrer" title={title}>
+                <i className={icon} />
+            </a>
+            <div className="link-content">
+                <h3>{title}</h3>
+                <p>{desc}</p>
+            </div>
+        </div>
+    );
+};
