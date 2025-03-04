17b3eab5088556eeb48ffc12d2dfa2a2d4721572
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-04 12:01:57 -0500

    refactor: remove unused navigation logic and state management from Home component

--- project2mdjlo/src/pages/Home.tsx
+++ project2mdjlo/src/pages/Home.tsx
@@ -1,28 +1,8 @@
 import { Header } from "../components/header";
 import { sectionData } from "../utils";
 import { SectionList } from "../components/SectionList";
-import { useEffect, useState } from "react";
-
-const navigate = (href) => {
-    window.history.pushState({}, "", href);
-    const navEvt = new Event('pushstate');
-    window.dispatchEvent(navEvt);
-}
-
 
 export const Home = () => {
-    const [currentPath, setCurrentPath] = useState<string>(window.location.pathname);
-
-    useEffect(() => {
-        const onLocationChange = () => {
-            setCurrentPath(window.location.pathname);
-        }
-        window.addEventListener('pushstate', onLocationChange);
-        return () => {
-            window.removeEventListener('pushstate', onLocationChange);
-        }
-    } , [])
-
 	return (
 		<main>
 			<Header />
