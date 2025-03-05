4c476f7b9601f7f4877d53dcbbe2e984d2071fcc
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-05 09:35:52 -0500

    refactor: simplify header layout and update CSS class usage

--- project2mdjlo/src/components/header.tsx
+++ project2mdjlo/src/components/header.tsx
@@ -2,8 +2,8 @@ import "../header.css";
 
 export const Header = () => {
 	return (
-		<header className="header shadow-md w-full bg-white py-4 px-4">
-            <nav className="nav-container max-w-7xl mx-auto flex justify-between items-center">
+		<header className="header shadow-md w-full bg-white py-4 pl-4">
+            <nav className="nav-container">
                 <div className="nav-content flex items-center gap-4">
                     <a className="logo-section flex items-center gap-4" href="https://www.munijlo.gob.pe/web/" target="_blank" rel="noreferrer">
                         <img 
@@ -11,7 +11,7 @@ export const Header = () => {
                             alt="Logo Municipal" 
                             className="nav-logo w-12 h-16 object-contain" //tamaño del logo
                         />
-                        <h3 className="nav-title text-secondary font-medium flex flex-col text-lg">
+                        <h3 className="font-(family-name:--titles) text-(--secondary-color) font-medium flex flex-col text-lg">
                             <span>Municipalidad Distrital de</span>
                             <span>José Leonardo Ortiz</span>
                         </h3>
