0ac2af6412784f0d1506c57f879be1e0d538e336
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-06 12:53:36 -0500

    feat(Footer): add Footer component with copyright information

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+	return (
+		<footer className="w-full bg-secondary py-4 px-4 flex items-center justify-center gap-2 text-white font-bold text-lg text-center font-body">
+			2025 © Municipalidad José Leonardo Ortiz - Subgerencia de
+			Tecnologías de la Información y Procesos informáticos
+		</footer>
+	);
+};
