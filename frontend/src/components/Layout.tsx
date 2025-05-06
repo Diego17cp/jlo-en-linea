7924f6443ec79c6a2d4a0ee31eabc4329ae8abb4
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-06 12:53:48 -0500

    feat(Layout): enhance layout structure with responsive header and footer

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,15 +1,31 @@
 import { Outlet } from "react-router";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { useLocation } from "react-router";
+import { MainContainer } from "./MainContainer";
 
 export const Layout = () => {
-    return (
-        <>
-            <Header />
-            <main>
-                <div className="container-main">
-                    <Outlet />
-                </div>
-            </main>
-        </>
-    );
-}
+	const location = useLocation();
+	const isHome = location.pathname === "/";
+	return (
+		<div className="flex flex-col min-h-screen w-full">
+			<Header />
+			<main className="flex-1 w-full">
+				{isHome && (
+					<picture>
+						{/* <source media="(max-width: 768px)" srcSet="/header-mobile.png" /> */}
+						<img
+							src="/header-example.jpg"
+							alt="Header"
+							className="w-full h-70 object-cover"
+						/>
+					</picture>
+				)}
+				<MainContainer>
+					<Outlet />
+				</MainContainer>
+			</main>
+			<Footer />
+		</div>
+	);
+};
