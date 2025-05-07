108ab791b680165871bf175eb951309c8805eed7
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-07 11:40:01 -0500

    refactor(App): restructure routes for improved organization and clarity

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
 import "./App.css";
-import { Layout } from "./components/Layout";
+import { Layout } from "./components/Layouts/Layout";
 import { Section } from "./pages/Section";
+import { SectionLayout } from "./components/Layouts/SectionLayout";
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Home />}></Route>
-					<Route path="seccion/:id" element={<Section />}></Route>
+					<Route path="seccion" element={<SectionLayout />}>
+                        <Route index element={<Section />} />
+                        <Route path=":id" element={<Section />} />
+                    </Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
