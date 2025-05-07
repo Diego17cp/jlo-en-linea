a189934717edf45da0e07db8ee3f41367d93da42
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-07 09:17:16 -0500

    feat(MainContainer): update padding based on route to enhance layout responsiveness

--- frontend/src/components/MainContainer.tsx
+++ frontend/src/components/MainContainer.tsx
@@ -1,6 +1,9 @@
+import { useLocation } from "react-router";
 export const MainContainer = ({ children }: { children: React.ReactNode }) => {
+	const location = useLocation();
+	const isHome = location.pathname === "/";
 	return (
-		<div className="p-0 md:p-8 bg-white rounded-xl w-semi-full min-h-1/2 h-auto flex items-center justify-center my-8 mx-auto">
+		<div className={`${isHome ? 'p-0' : 'p-8'} md:p-8 bg-white rounded-xl w-semi-full min-h-1/2 h-auto flex items-center justify-center my-8 mx-auto`}>
 			{children}
 		</div>
 	);
