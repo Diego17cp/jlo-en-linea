b7b019dbba19e64179a76454442926183f9b5051
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-07 08:49:05 -0500

    style(MainContainer): adjust padding for improved layout responsiveness

--- frontend/src/components/MainContainer.tsx
+++ frontend/src/components/MainContainer.tsx
@@ -1,6 +1,6 @@
 export const MainContainer = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="p-8 bg-white rounded-xl w-semi-full min-h-1/2 h-auto flex items-center justify-center my-8 mx-auto">
+		<div className="p-0 md:p-8 bg-white rounded-xl w-semi-full min-h-1/2 h-auto flex items-center justify-center my-8 mx-auto">
 			{children}
 		</div>
 	);
