a3d69645b20ec29c4df975b5aca131f38b1fb413
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-07 12:01:03 -0500

    refactor(SectionLayout): adjust text and image sizes for improved layout consistency

--- frontend/src/components/Layouts/SectionLayout.tsx
+++ frontend/src/components/Layouts/SectionLayout.tsx
@@ -9,20 +9,20 @@ export const SectionLayout = () => {
 			<section className="mb-10 ml-5 flex gap-5 w-full md:w-2/3 justify-center md:justify-start items-center">
 				<Link
 					to="/"
-					className="text-3xl md:text-4xl text-primary font-titles hover:text-secondary transition-all duration-300 ease-in"
+					className="text-lg md:text-4xl text-primary font-titles hover:text-secondary transition-all duration-300 ease-in"
 				>
 					<i className="fa-solid fa-house mr-2 align"></i>
 					Inicio
 				</Link>
 				<div className="bg-primary w-1 h-10 rounded-full"></div>
 				{section && (
-					<div className="flex gap-5 items-center">
+					<div className="flex gap-2 items-center">
 						<img
 							src={section.iconUrl}
 							alt={section.title}
-							className="w-10 h-10 md:w-12 md:h-12"
+							className="w-8 h-8 md:w-12 md:h-12"
 						/>
-						<span className="text-2xl md:text-3xl text-secondary font-titles">
+						<span className="text-lg md:text-3xl text-secondary font-titles">
 							{section.title}
 						</span>
 					</div>
