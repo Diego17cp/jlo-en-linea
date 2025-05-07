618ff1840ca3c9e4aa17cdb152979f22bbdee2f2
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-07 11:05:55 -0500

    refactor(ServiceCard): enhance layout and styling for improved visual consistency

--- frontend/src/components/Cards/ServiceCard.tsx
+++ frontend/src/components/Cards/ServiceCard.tsx
@@ -3,13 +3,13 @@ import { Section } from "../../types";
 
 export const ServiceCard = ({ id, title, iconUrl, href } : Omit<Section, 'desc'>) => {
     return (
-        <Link to={href} className="group-hover:opacity-60 hover:opacity-100 py-8 px-8 flex items-center flex-col border-2 border-primary transition-all duration-300 ease-in-out rounded-lg shadow-md hover:shadow-lg hover:scale-110">  
-            <div className="w-full rounded-full border border-tertiary h-2/3 flex justify-center items-center transition-all duration-300 ease-in-out group-hover:shadow-lg">
-                <img src={iconUrl} alt={title} className="w-full h-full object-cover" />
+        <Link to={href} className="group-hover:opacity-60 hover:opacity-100 py-8 gap-8 px-5 flex flex-col items-center border-2 border-primary transition-all duration-300 ease-in-out rounded-lg shadow-md hover:shadow-lg hover:scale-110">  
+            <div className="w-40 h-40 rounded-full border border-tertiary flex justify-center items-center bg-white transition-all duration-300 ease-in-out group-hover:shadow-lg mb-2">
+                <img src={iconUrl} alt={title} className="w-24 h-20 object-cover" />
             </div>
-            <div className="flex justify-center items-center mt-6 h-1/3 w-full">
-                <h3 className="m-0 text-center text-secondary font-titles">{title}</h3>
-            </div>
+            <h3 className="mt-2 text-center text-secondary font-titles text-lg font-semibold w-full break-words leading-tight">
+                {title}
+            </h3>
         </Link>
     )
 }
