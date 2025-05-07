45b29d8b964d3c546ef145d341ad79dd118c855f
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-07 11:40:36 -0500

    refactor(MainContainer): update layout to ensure children are displayed in a column

--- frontend/src/components/MainContainer.tsx
+++ frontend/src/components/MainContainer.tsx
@@ -3,7 +3,7 @@ export const MainContainer = ({ children }: { children: React.ReactNode }) => {
 	const location = useLocation();
 	const isHome = location.pathname === "/";
 	return (
-		<div className={`${isHome ? 'p-0' : 'p-8'} md:p-8 bg-white rounded-xl w-semi-full min-h-1/2 h-auto flex items-center justify-center my-8 mx-auto`}>
+		<div className={`${isHome ? 'p-0' : 'p-8'} md:p-8 bg-white rounded-xl w-semi-full min-h-1/2 h-auto flex items-center justify-center flex-col my-8 mx-auto`}>
 			{children}
 		</div>
 	);
