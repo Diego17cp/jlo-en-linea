441fc276c6d7f060679a0753bf1b788ccd4f9a6f
Diego17 <diegoalbertocastropastor@gmail.com>
2025-05-07 08:48:37 -0500

    style(Footer): adjust text size from lg to base for improved readability

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
 	return (
-		<footer className="w-full bg-secondary py-4 px-4 flex items-center justify-center gap-2 text-white font-bold text-lg text-center font-body">
+		<footer className="w-full bg-secondary py-4 px-4 flex items-center justify-center gap-2 text-white font-bold text-base md:text-lg text-center font-body">
 			2025 © Municipalidad José Leonardo Ortiz - Subgerencia de
 			Tecnologías de la Información y Procesos informáticos
 		</footer>
