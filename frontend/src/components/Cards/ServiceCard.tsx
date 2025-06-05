0167558331ebe60c622d8f72b82078e6bbb4e71c
Diego17 <diegoalbertocastropastor@gmail.com>
2025-06-04 20:56:12 -0500

    refactor(ServiceCard): remove unused 'id' prop from ServiceCard component refactor(types): make Section interface declaration consistent

--- frontend/src/components/Cards/ServiceCard.tsx
+++ frontend/src/components/Cards/ServiceCard.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router";
-import { Section } from "../../types";
 
-export const ServiceCard = ({ id, title, iconUrl, href } : Omit<Section, 'desc'>) => {
+export const ServiceCard = ({ title, iconUrl, href } : Omit<Section, 'desc' | 'id'>) => {
     return (
         <Link to={href} className="group-hover:opacity-60 hover:opacity-100 py-8 gap-8 px-5 flex flex-col items-center border-2 border-primary transition-all duration-300 ease-in-out rounded-lg shadow-md hover:shadow-lg hover:scale-110">  
             <div className="w-40 h-40 rounded-full border border-tertiary flex justify-center items-center bg-white transition-all duration-300 ease-in-out group-hover:shadow-lg mb-2">
--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,4 +1,4 @@
-export interface Section {
+interface Section {
     id: string;
     title: string;
     desc: string;
