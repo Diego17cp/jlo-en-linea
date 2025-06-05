3cbbd50b488cfb3e255fa48beb6dec08dbe17609
Diego17 <diegoalbertocastropastor@gmail.com>
2025-06-04 21:33:09 -0500

    refactor(ServiceCard): update image and container styles for consistency

--- frontend/src/components/Cards/ServiceCard.tsx
+++ frontend/src/components/Cards/ServiceCard.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 export const ServiceCard = ({ title, iconUrl, href } : Omit<Section, 'desc' | 'id'>) => {
     return (
         <Link to={href} className="group-hover:opacity-60 hover:opacity-100 py-8 gap-8 px-5 flex flex-col items-center border-2 border-primary transition-all duration-300 ease-in-out rounded-lg shadow-md hover:shadow-lg hover:scale-110">  
-            <div className="w-40 h-40 rounded-full border border-tertiary flex justify-center items-center bg-white transition-all duration-300 ease-in-out group-hover:shadow-lg mb-2">
-                <img src={iconUrl} alt={title} className="w-24 h-20 object-cover" />
+            <div className="size-40 rounded-full border border-tertiary flex justify-center items-center bg-white transition-all duration-300 ease-in-out group-hover:shadow-lg mb-2">
+                <img src={iconUrl} alt={title} className="max-w-[70%] max-h-[70%] object-contain" />
             </div>
             <h3 className="mt-2 text-center text-secondary font-titles text-lg font-semibold w-full break-words leading-tight">
                 {title}
