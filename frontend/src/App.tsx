72087f61b30b574dd5c3b84517634ed3c7938cbc
Diego17 <diegoalbertocastropastor@gmail.com>
2025-07-11 12:55:43 -0500

    feat(routes): add NotFound route to all sections for improved error handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,30 +5,41 @@ import { Layout } from "./components/Layouts/Layout";
 import { Section } from "./pages/Section";
 import { SectionLayout } from "./components/Layouts/SectionLayout";
 import { Consultas } from "./components/Consultas";
+import { NotFound } from "./components/404";
+
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Home />}></Route>
-					<Route path="seccion" element={<SectionLayout />}>
-						<Route index element={<Section />} />
-						<Route path=":id" element={<Section />} />
-					</Route>
-				</Route>
-				<Route path="licencias">
-					<Route path="consulta">
-						<Route path=":id" element={<Consultas />} />
-					</Route>
-				</Route>
-				<Route path="transito">
-					<Route path="consulta">
-						<Route path=":id" element={<Consultas />} />
-					</Route>
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />}></Route>
+                    <Route path="seccion" element={<SectionLayout />}>
+                        <Route index element={<Section />} />
+                        <Route path=":id" element={<Section />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+                
+                <Route path="licencias">
+                    <Route path="consulta">
+                        <Route path=":id" element={<Consultas />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+                
+                <Route path="transito">
+                    <Route path="consulta">
+                        <Route path=":id" element={<Consultas />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Route>                
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
